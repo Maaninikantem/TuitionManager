9b61793a7968c63885369feebf77779149b71117
maaninikantem <maaninikantem@gmail.com>
2023-02-26 23:01:48 -0500

    changes

--- Project2/src/Enrollment.java
+++ Project2/src/Enrollment.java
@@ -1,3 +1,7 @@
+/**
+ * This class defines the list of the students who are enrolled in classes this semester
+ * @author Maanini Kantem, Michael Mankiewicz
+ */
 public class Enrollment {
 
     private EnrollStudent[] enrollStudents;
@@ -12,9 +16,8 @@ public class Enrollment {
         enrollStudents = newEnrollStudents;
     } //increase the array capacity by 4
     /**
-     This method will add a new student element into the roster.
-     @param student the student to add.
-     @return false if student already in roster.
+     This method will add a registered student into the enrollment.
+     @param enrollStudent the student to add.
      */
 
     public void add(EnrollStudent enrollStudent){
@@ -36,13 +39,21 @@ public class Enrollment {
 
     //move the last one in the array to replace the deleting index position
 
-
+    /**
+     * This method removes the registered student from the enrollment
+     * @param enrollStudent the student to remove
+     */
     public void remove(EnrollStudent enrollStudent){
         int indexOfEnrollStudent = find(enrollStudent);
         enrollStudents[indexOfEnrollStudent] = enrollStudents[size-1];
         size--;
     }
 
+    /**
+     * this method is created to help find the enrolled students in the list
+     * @param enrollStudent to find the index of the element
+     * @return the index of found, -1 if the enrollStudent was not found
+     */
     private int find(EnrollStudent enrollStudent) {
         for(int i = 0; i < size; i++){
             if(enrollStudents[i].equals(enrollStudent)){
@@ -51,6 +62,12 @@ public class Enrollment {
         }
         return -1;
     }
+
+    /**
+     * This method was created to find the enrollStudent based on their profile
+     * @param profile the profile of the enrollStudent
+     * @return the index of found, -1 if the enrollStudent's profile was not found
+     */
     public int findProfile(Profile profile){
         for(int i = 0; i< size; i++){
             if(enrollStudents[i].getProfile().equals(profile)){
@@ -59,6 +76,12 @@ public class Enrollment {
         }
         return -1;
     }
+
+    /**
+     * this method helps us get the enrollStudent we are tryign to access by the index
+     * @param profile based on the profile of the enrollStudent
+     * @return return the index of the enrollStudent if found, null if not found
+     */
     public EnrollStudent getEnrollStudent(Profile profile){
         int studentIndex = findProfile(profile);
         if(studentIndex == -1){
@@ -66,9 +89,19 @@ public class Enrollment {
         }
         return enrollStudents[studentIndex];
     }
+
+    /**
+     * this method uses the find method to let the user know if the student is in the list or not
+     * @param enrollStudent based on the input it searches for the student in the list
+     * @return true or false based on found or not found
+     */
     public boolean contains(EnrollStudent enrollStudent){
         return find(enrollStudent) != -1;
     }
+
+    /**
+     * this method is used to print the list of enrolledStudents in the list so far, based on their profiles, credits enrolled
+     */
     public void print() {
         String print = "";
         if (size == 0) {
@@ -84,6 +117,11 @@ public class Enrollment {
         System.out.print(print);
         System.out.println("* end of enrollment **");
     }//print the array as is without sorting
+
+    /**
+     * This method prints the enrolledStudents tuition due based on their status and the credits they are taking
+     * @param roster takes the students from the roster and if they are in the enroll list, it will display the tuition due
+     */
     public void printTuition(Roster roster) {
         String print = "";
         if (size == 0) {
@@ -100,6 +138,11 @@ public class Enrollment {
         System.out.println("* end of enrollment **");
     }
 
+    /**
+     * This method adds the enrolled credits of the student to the total credits completed
+     * then prints out all the students who have finished the credits over 120 and are eligible for graduation
+     * @param roster
+     */
     public void semesterEnd(Roster roster){
         for(int i = 0; i <= size; i++) {
             EnrollStudent currEnrollStudent = enrollStudents[i];
@@ -112,6 +155,11 @@ public class Enrollment {
         printSemEnd(roster);
     }
 
+    /**
+     * this is helper method for the semesterEnd method
+     * this method is then called on the semesterEnd method
+     * @param roster it takes in the roster's student instance and prints the students based on the credits completed
+     */
     public void printSemEnd(Roster roster){
 
             String print = "";
